343e7b62f9afef2170db8338f30f6d85d0af42cf
yukishi24 <yukishi24github@gmail.com>
2023-01-08 16:12:08 +0900

    add: Main

---
+++ Hello/src/main/java/hello/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package hello;
+
+/**
+ * @author yukishi
+ *
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+
+}
